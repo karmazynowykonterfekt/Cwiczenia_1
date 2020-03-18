72757bcc740907b21bd3492b1b8e737d06e24a2c
karmazynowykonterfekt <kasia_michalak@op.pl>
2020-03-18 22:08:36 +0100

    Updated

--- Cwiczenia_1/Program.cs
+++ Cwiczenia_1/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Cwiczenia_1
+namespace Tablice
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //początek
+            Console.Write("Czesc! Ile liczb chcesz zsumować?: ");
+            string snumber = Console.ReadLine();
+
+            int[] numbers = new int[Convert.ToInt32(snumber)];
+
+            //pobieranie liczb od użytkownika
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                string numberofanumber = (i + 1).ToString();
+                Console.Write("Podaj" + numberofanumber + "liczbę: ");
+                string s = Console.ReadLine();
+                numbers[i] = Convert.ToInt32(s);
+            }
+            //wyświetlanie podanych liczb
+            Console.Write("Podane przez Ciebie liczby to: ");
+            for (int i = 0; i < numbers.Length; i++)
+                Console.Write(numbers[i].ToString() + ", ");
+            Console.WriteLine();
+
+            //obliczanie sumy z podanych liczb
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+                sum += numbers[i];
+            Console.WriteLine("Suma liczb: " + sum.ToString());
+            Console.ReadKey();
         }
+
     }
 }
