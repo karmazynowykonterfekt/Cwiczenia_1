1803731ab465edaa49d8a8ba95ecd4889f6a74e7
karmazynowykonterfekt <kasia_michalak@op.pl>
2020-03-18 21:45:12 +0100

    Add project files.

---
+++ Cwiczenia_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cwiczenia_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
